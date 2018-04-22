71b507ddec23dd2ff2cfeb43e8a945f5f9ca6820
smartdev212 <smartdev212@gmail.com>
2018-04-21 23:44:01 -0400

    Work

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,7 @@ import Header from './Header';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
+const Landing = () => <div className="container"><h2>Landing</h2></div>;
 
 const App = () => {
     return (
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
+        <nav>
+            <div class="nav-wrapper">
+            <a href="#" className="brand-logo left">ReactMail</a>
+            <ul className="right">
+                <li><a>Login</a></li>
+            </ul>
             </div>
+        </nav>
         );
     }
 }
