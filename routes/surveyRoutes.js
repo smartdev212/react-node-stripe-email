8554303f080215d31ce5237d267b9019710f7fdb
smartdev212 <smartdev212@gmail.com>
2018-05-11 23:46:46 -0400

    Add production route in emails. Persist sent surveys to DB

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
   });
  
 
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -29,11 +29,11 @@ module.exports = (app) => {
     
     try{
       await mailer.send();
-      await survey.save;
+      await survey.save();
       req.user.credits -= 1;
       const user = await req.user.save();
 
-      res.send(users);
+      res.send(user);
     } catch (err) {
       res.status(422).send(err);
     }
