c87c2b18293acd7f7b182ba179d5386c089db36e
smartdev212 <smartdev212@gmail.com>
2018-05-13 14:37:36 -0400

    Install redux-form. Start building SurveyNew component and subcomponents

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,9 +6,9 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing';
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
 
 class App extends Component {
     
---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class SurveyForm extends Component {
+    render() {
+        return (
+            <div>
+                SurveyForm
+            </div>
+        )
+    }
+}
+
+
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyForm);
