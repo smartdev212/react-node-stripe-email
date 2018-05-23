4cbc56dc2c4a5b7a89f0fd233069e8695cc85c05
smartdev212 <smartdev212@gmail.com>
2018-05-22 22:56:36 -0400

    Create SurveyField component

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+
+    renderFields () {
+        return (
+            <div>
+                <Field label="Survey Title" type="text" name="Title" component={SurveyField} />
+                <Field label="Subject Line" type="text" name="Subject" component={SurveyField} />
+                <Field label="Email Body" type="text" name="Email_Body" component={SurveyField} />
+                <Field label="Recipient List" type="text" name="Recipients" component={SurveyField} />
+
+            </div>
+        )
+    }
+
     render() {
         return (
             <div>
                 <form onSubmit={this.props.handleSubmit(
                     values => console.log(values)
                 )}>
-                    <Field
-                    type="text"
-                    name="surveyTitle"
-                    component="input"
-                    />
+                    {this.renderFields()}
                     <button type="submit">Submit</button>
                 </form>
             </div>
