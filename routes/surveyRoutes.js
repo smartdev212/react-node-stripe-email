4a945c7743efc7c712bd078943954d245b1f7d4c
smartdev212 <smartdev212@gmail.com>
2018-06-07 23:51:07 -0400

    Set up localtunnel and Sendgrid webhook config

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -12,6 +12,11 @@ module.exports = (app) => {
     res.send('Thank you for giving feedback!');
   });
  
+  app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
+    res.send({});
+  })
+
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
